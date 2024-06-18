0778374bb991320761d90fd64beade9eb2855998
MatrixC <blueicexfx@gmail.com>
2024-06-18 11:01:32 +0800

    add miniz source code

--- xmake.lua
+++ xmake.lua
@@ -1,20 +1,25 @@
 add_rules("mode.debug", "mode.release")
-add_requires("fmt", "miniz")
+add_requires("fmt")
 
 set_languages("c99", "cxx23")
 set_optimize("none")
-
 
 target("RexVM")
     set_kind("binary")
     set_toolchains("clang")
     set_toolset("debugger", "lldb")
+    add_includedirs("src/third_party/miniz")
     add_files(
         "src/*.cpp",
         "src/utils/*.cpp",
-        "src/native/*.cpp"
+        "src/native/*.cpp",
+
+        "src/third_party/miniz/miniz.c",
+        "src/third_party/miniz/miniz_zip.c",
+        "src/third_party/miniz/miniz_tinfl.c",
+        "src/third_party/miniz/miniz_tdef.c"
     )
-    add_packages("fmt", "miniz")
+    add_packages("fmt")
     set_warnings("all", "error")
 
     -- set_policy("build.sanitizer.address", true)
